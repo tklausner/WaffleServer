1132caa65541c34c395d0d4d1a44cd3a60de8b7d
Teddy <klausnerteddy@gmail.com>
2020-07-03 17:41:00 -0700

    updated post

--- controllers/post.js
+++ controllers/post.js
@@ -74,22 +74,14 @@ const fetchPostsByUser = async (req, res) => {
   }
 };
 
-function waffleRandomizer(wafflers) {
-  const size = wafflers.length;
-  console.log("SIZE", size);
-  let r = int(random(0, size));
-  console.log("R", r);
-  console.log("wafflers[r]", wafflers[r]);
-  return wafflers[r];
-}
-
 const fetchWaffleWinner = async (req, res) => {
   try {
     const { id } = req.params;
     const post = await Post.findById(id);
     const { wafflers } = post;
-    let winner = waffleRandomizer(wafflers);
-    return winner;
+    const size = wafflers.length;
+    let r = int(random(0, size));
+    return wafflers[r];
   } catch (err) {
     console.log(err);
   }
