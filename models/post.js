4a0143ad5100261a506c99588e8de49fadcaef08
Teddy <klausnerteddy@gmail.com>
2020-07-04 22:14:45 -0700

    added timestamp to post

--- models/post.js
+++ models/post.js
@@ -2,20 +2,25 @@
 var mongoose = require("mongoose");
 
 // Define POST SCHEMA
-var PostSchema = new mongoose.Schema({
-  user_id: String,
-  username: String,
-  category: { type: String, default: "none" },
-  image: { type: String },
-  profile: { type: String },
-  description: String,
-  value: { type: Number, default: 0 },
-  main_spots: { type: Number, default: 0 },
-  main_price: { type: Number, default: 0 },
-  waffles_remaining: { type: Number, default: 0 },
-  comments: { type: [mongoose.Schema.Types.ObjectId], ref: "Comment" },
-  wafflers: { type: [mongoose.Schema.Types.ObjectId], ref: "User" },
-});
+var PostSchema = new mongoose.Schema(
+  {
+    user_id: String,
+    username: String,
+    category: { type: String, default: "none" },
+    image: { type: String },
+    profile: { type: String },
+    description: String,
+    value: { type: Number, default: 0 },
+    main_spots: { type: Number, default: 0 },
+    main_price: { type: Number, default: 0 },
+    waffles_remaining: { type: Number, default: 0 },
+    comments: { type: [mongoose.Schema.Types.ObjectId], ref: "Comment" },
+    wafflers: { type: [mongoose.Schema.Types.ObjectId], ref: "User" },
+  },
+  {
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+  }
+);
 
 // Export the Mongoose model
 
