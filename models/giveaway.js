dbcdd9df2a87f67cd97f3c7f7561a7f0e770df5c
Teddy <klausnerteddy@gmail.com>
2020-07-28 15:25:43 -0700

    updated giveaway sche,a

--- models/giveaway.js
+++ models/giveaway.js
@@ -4,6 +4,9 @@ var mongoose = require("mongoose");
 // Define GIVEAWAY SCHEMA
 var GiveawaySchema = new mongoose.Schema({
   users: { type: [mongoose.Schema.Types.ObjectId], ref: "User" },
+  start_time: Date,
+  duration: Number,
+  end_time: Date,
 });
 
 // Export the Mongoose model
