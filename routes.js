ec211a3d3cade481208adfe20219e002a21d5a16
Teddy <klausnerteddy@gmail.com>
2020-07-28 15:17:47 -0700

    added giveaway to routes{

--- routes.js
+++ routes.js
@@ -7,6 +7,14 @@ const {
   fetchUserByFBId,
 } = require("./controllers/user");
 
+const {
+  fetchGiveaways,
+  fetchGiveawayById,
+  addGiveaway,
+  deleteGiveaway,
+  updateGiveaway,
+} = require("./controllers/giveaway");
+
 // ALL API ROUTES
 
 const routes = [
@@ -39,6 +47,31 @@ const routes = [
     method: "GET",
     url: "/api/users/fb/:fb_id",
     handler: fetchUserByFBId,
+  },
+  {
+    method: "GET",
+    url: "/api/giveaways",
+    handler: fetchGiveaways,
+  },
+  {
+    method: "POST",
+    url: "/api/giveaways",
+    handler: addGiveaway,
+  },
+  {
+    method: "DELETE",
+    url: "/api/giveaways/:id",
+    handler: deleteGiveaway,
+  },
+  {
+    method: "PUT",
+    url: "/api/giveaways/:id",
+    handler: updateGiveaway,
+  },
+  {
+    method: "GET",
+    url: "/api/giveaways/:id",
+    handler: fetchGiveawayById,
   },
 ];
 
